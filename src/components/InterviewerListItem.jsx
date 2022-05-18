3ef690d089298f9cd62e92e0f8a801d2bc16f428
Matthieu Pauze <matt.pauze@gmail.com>
2022-05-18 16:24:40 -0400

    Implement InterviewerListItem CSS & JSX

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import "components/styles/InterviewerListItem.scss"
+import classNames from 'classnames';
 
-const InterviewerListItem = () => {
+const InterviewerListItem = (props) => {
+  const { id, name, avatar, selected, setInterviewer } = props;
+
+  const interviewerClasses = classNames({
+    "interviewers__item": true,
+    "interviewers__item--selected": selected,
+  });
+
   return (
-    <div>InterviewerListItem</div>
+    <li
+      onClick={() => setInterviewer && setInterviewer(id)}
+      className={interviewerClasses}>
+      <img
+        className='interviewers__item-image'
+        src={avatar}
+        alt={name}
+      />
+      {selected && name}
+    </li>
   )
 }
 
