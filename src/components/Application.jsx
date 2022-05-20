75af12a2af220d3c5b660e55350bd9c93cf9421c
Matthieu Pauze <matt.pauze@gmail.com>
2022-05-20 18:22:18 -0400

    Move URLS to be proxy based

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,28 +3,34 @@ import axios from "axios";
 
 import "components/styles/Application.scss";
 
-import { appointments } from "data/appointments"
-
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   })
-  const setDay = day => setState({...state, day});
-  const setDays = days => setState(prev => ({...prev, days}));
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const setDay = day => setState(prev => ({ ...prev, day }));
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days")
-      .then(res => {
-        setDays(res.data);
-      })
-      .catch(err => console.log(err));
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(all => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    })
   }, [])
 
+  const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return <Appointment key={appointment.id} {...{...appointment, interview}} />
+  })
 
   return (
     <main className="layout">
@@ -47,9 +53,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((appt) => {
-          return <Appointment key={appt.id} {...appt} />
-        })}
+        {schedule}
       </section>
     </main>
   );
