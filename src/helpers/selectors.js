2c228ee4556aa2e06937c1434a482deed3db920a
Matthieu Pauze <matt.pauze@gmail.com>
2022-05-23 19:03:35 -0400

    Fix: Fix bug where we used the wrong interviewers list

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,26 +1,15 @@
-export const getAppointmentsForDay = (state, day) => {
-  const theDay = state.days.find(date => date.name === day);
-
+export const getAppointmentsForDay = (state, selectedDay) => {
+  const theDay = state.days.find(date => date.name === selectedDay);
   if (!theDay) {
     return [];
   }
-
-  return theDay.appointments
-    .map(appt => state.appointments[appt]);
+  return theDay.appointments.map(appt => state.appointments[appt]);
 }
 
-export const getInterviewersForDay = (state, day) => {
-  const { interviewers } = state
-  const dailyInterviewers = {};
-
-  getAppointmentsForDay(state, day)
-    .filter(appt => appt.interview)
-    .map(appt => appt.interview.interviewer)
-    .forEach(interviewer => {
-      dailyInterviewers[interviewer] = interviewers[interviewer]
-    });
-
-  return Object.values(dailyInterviewers);
+export const getInterviewersForDay = (state, selectedDay) => {
+  const today = state.days.find(day => day.name === selectedDay);
+  const interviewerIds = today.interviewers;
+  return interviewerIds.map(id => state.interviewers[id]);
 }
 
 export const getInterview = (state, interview) => {
